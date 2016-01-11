e565c9128db296047a83c2674a8be8ebcf9b33ce
midnitesun002 <jrtinsley105@gmail.com>
2016-01-10 23:18:23 -0500

    dijkstra algorithm

---
+++ dijkstra.py
@@ -0,0 +1,35 @@
+
+class Node:
+ def __init__(self, id=''):
+  self.id = id
+  self.attachedNodes = list()
+ 
+ def addNode(self, pair):
+  # pair is a node and distance to node tuple
+  self.attachedNodes.append(pair)
+  
+ def getNodes(self):
+  return self.attachedNodes
+  
+ def getID(self):
+  return self.id
+
+# Create nodes
+N = 5
+nodes = [Node(str(i)) for i in range(N * N)]
+
+# Arrange nodes in a grid, equidistant
+for i in range(N * N - 1):
+ if (i + 1) % N != 0:
+  nodes[i].addNode((nodes[i + 1], 1))
+ if i // N < N - 1:
+  nodes[i].addNode((nodes[i + N], 1))
+
+# Display nodes`
+for i in range(N * N):
+ attached = ''
+ for node in nodes[i].getNodes():
+  attached += node[0].getID() + ' '
+ print('Node %d is attached to %s' % (i, attached))
+
+startNode = nodes[0]
